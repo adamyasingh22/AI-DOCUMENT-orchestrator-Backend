e4492fa7f58053f6a4ce4a40a389284d6fef4af5
Adamya singh <adamyasingh22@gmail.com>
2025-11-21 20:14:13 +0530

    update cors in server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 
@@ -7,7 +6,26 @@ const uploadRoutes = require('./routes/upload');
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    const allowed = ['http://localhost:3000', 'http://127.0.0.1:3000'];
+    if (allowed.includes(origin)) return callback(null, true);
+    return callback(new Error('CORS not allowed by server'), false);
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+app.use((req, res, next) => {
+  console.log('Incoming origin:', req.headers.origin);
+  next();
+});
+
 app.use(express.json({ limit: '10mb' }));
 
 app.use('/api', uploadRoutes);
@@ -16,4 +34,3 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
